bf81fb36eb0aaf55646c2a763a12f0741c758f97
Julian <jsuidgeest@gmail.com>
2020-01-30 14:11:02 +0100

    asset fix

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/profile/{username}", name="user_profile")
+     */
+    public function userProfile($username)
+    {
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['username' => $username]);
+        return $this->render('profile/index.html.twig', ['user' => $user]);
+    }
+}
